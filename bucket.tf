b5ec11314bd94f29a906853d6cfeda672cf8d414
Yago Almeida <almeida_yago@hotmail.com>
2022-05-15 12:36:58 -0300

    fix bucket

--- bucket.tf
+++ bucket.tf
@@ -1,9 +1,4 @@
-locals {
-    acl = "private"
-    bucket_name = "shared_storage_tfstate"
-}
-
-resource "aws_s3_bucket" "shared_storage_tfstate" {
-    bucket = local.bucket_name
-    acl    = local.acl
+resource "aws_s3_bucket" "shared-storage-terraform-state" {
+  bucket = "shared-storage-terraform-state-dev"
+  acl    = "private"
 }
