12b55b0906d1fa144bca6460302185dac20b5fd1
Yago Almeida <almeida_yago@hotmail.com>
2022-05-15 13:18:43 -0300

    fix security issues

--- dynamo.tf
+++ dynamo.tf
@@ -1,4 +1,5 @@
 resource "aws_dynamodb_table" "terraform_state_lock" {
+  depends_on     = [aws_kms_key.kms]
   name           = "terraform-lock"
   read_capacity  = 5
   write_capacity = 5
@@ -6,5 +7,12 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.kms.arn
+  }
+  point_in_time_recovery {
+    enabled = true
   }
 }
