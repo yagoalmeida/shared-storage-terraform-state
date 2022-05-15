1e99e3aaab7b79dec346c86cc34eb4715d00c9fb
Yago Almeida <almeida_yago@hotmail.com>
2022-05-15 12:46:14 -0300

    add  log bucket

--- bucket.tf
+++ bucket.tf
@@ -1,4 +1,24 @@
 resource "aws_s3_bucket" "shared-storage-terraform-state" {
   bucket = "shared-storage-terraform-state-dev"
+}
+
+resource "aws_s3_bucket_acl" "shared-storage-terraform-state-acl" {
+  bucket = aws_s3_bucket.shared-storage-terraform-state.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket" "log-bucket" {
+  bucket = "log-bucket"
+}
+
+resource "aws_s3_bucket_acl" "log-bucket-acl" {
+  bucket = aws_s3_bucket.log-bucket.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "log-bucket-logging" {
+  bucket = aws_s3_bucket.shared-storage-terraform-state.id
+
+  target_bucket = aws_s3_bucket.log-bucket.id
+  target_prefix = "log/"
 }
